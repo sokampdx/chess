a0b93e19ef38f592c909f62b79e7d3a018b21a1f
sokampdx <sokam@pdx.edu>
2017-05-04 22:20:52 -0700

    move expect board position to file

--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -1,34 +1,14 @@
 require 'spec_helper'
 
-INITIAL_BOARD_SETUP= <<EOF
- a b c d e f g h
------------------
-|R|N|B|Q|K|B|N|R| 8
------------------
-|P|P|P|P|P|P|P|P| 7
------------------
-| | | | | | | | | 6
------------------
-| | | | | | | | | 5
------------------
-| | | | | | | | | 4
------------------
-| | | | | | | | | 3
------------------
-|p|p|p|p|p|p|p|p| 2
------------------
-|r|n|b|q|k|b|n|r| 1
------------------
-EOF
-
 describe Game do
   let(:game) { Game.new }
+  let(:init_position) { File.read("#{File.dirname(__FILE__)}/supports/initial_game_position") }
 
   it 'creates a new game' do
     game = Game.new
   end
 
   it 'starts with an initial board setup' do
-    expect(game.display_position).to eq(INITIAL_BOARD_SETUP)
+    expect(game.display_position).to eq(init_position)
   end
 end
