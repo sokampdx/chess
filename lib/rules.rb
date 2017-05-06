851d883b2e8dbd8c82917fe4806ad3b2cc01455a
sokampdx <sokam@pdx.edu>
2017-05-06 00:29:56 -0700

    added basic rules for moves

---
+++ lib/rules.rb
@@ -0,0 +1,24 @@
+module Rules
+  def move(init_position, a_move)
+    from, to = parse_move(a_move)
+
+    next_position = init_position
+    piece = init_position[from]
+    next_position[from] = " "
+    next_position[to] = piece 
+
+    next_position
+  end
+
+  def parse_move(a_move)
+    from, to = a_move.downcase.scan(/.{2}/)
+    return parse_index(from), parse_index(to)
+  end 
+
+  def parse_index(square)
+    x, y = square.split('')
+    x = x.ord - 'a'.ord
+    y = y.to_i - 1
+    y * 8 + x
+  end
+end
---
+++ spec/rules_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Rules do
+  let(:init_position) { 'rnbqkbnrpppppppp                                PPPPPPPPRNBQKBNR' }
+  let(:init_position) { 'rnbqkbnrpppppppp            P                   PPPP PPPRNBQKBNR' }
+  let(:norm_position) { 'rnbqkbnrpppp ppp            p                   PPPPPPPPRNBQKBNR' }
+  let(:game) { Class.new { include Rules } }
+
+  context '.move' do
+    let(:normal_move) { 'e2e4' }
+    
+    xit 'moves normal pieces' do
+      expect(game.new.move(init_position, normal_move)).to eq(norm_position)
+    end
+
+    xit 'moves ' do
+      expect(game.new.move(init_position, a_move)).to eq()
+    end
+  end
+end
