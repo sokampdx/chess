5c97dc805f24542568c15fc6fa0733cab644d1f9
sokampdx <anthonykpso@gmail.com>
2017-09-27 22:26:40 -0700

    create basic game turns

--- lib/chess_game.rb
+++ lib/chess_game.rb
@@ -2,12 +2,33 @@
 
 module ChessGame
   class Game
+    attr_reader :turn, :player
+
     def initialize
+      @turn = 0
+      @player = 'White'
     end
 
     def display_position
       board = Board.new
       board.display
+    end
+
+    def move(chess_move)
+      update_game if check_move(chess_move)
+    end
+
+    def check_move(chess_move)
+      true
+    end
+
+    def update_game
+      if @player == 'White'
+        @player = 'Black'
+      else
+        @player = 'White'
+        @turn += 1
+      end
     end
   end
 end
--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -8,7 +8,42 @@ describe Game do
     game = Game.new
   end
 
-  it 'starts with an initial board setup' do
-    expect(game.display_position).to eq(init_position)
+  describe '.display_position' do
+    it 'starts with an initial board setup' do
+      expect(game.display_position).to eq(init_position)
+    end
+  end
+
+  describe '.turn' do
+    it 'starts with turn 0' do
+      expect(game.turn).to eq(0)
+    end
+  end
+
+  describe '.player' do
+    it 'start with white' do
+      expect(game.player).to eq('White')
+    end
+  end
+
+  describe '.move' do
+    describe 'when given valid moves' do
+
+      let(:valid_move1) { 'e2e4' }
+      let(:valid_move2) { 'e7e5' }
+      let(:valid_move3) { 'd2d4' }
+
+      it 'changes alternating player color' do
+        expect { game.move(valid_move1) }.to change { game.player }.from('White').to('Black')
+        expect { game.move(valid_move2) }.to change { game.player }.from('Black').to('White')
+        expect { game.move(valid_move3) }.to change { game.player }.from('White').to('Black')
+      end
+
+      it 'changes turns only after black moves' do
+        expect { game.move(valid_move1) }.to_not change { game.turn }
+        expect { game.move(valid_move2) }.to change { game.turn }.from(0).to(1)
+        expect { game.move(valid_move3) }.to_not change { game.turn }
+      end
+    end
   end
 end
